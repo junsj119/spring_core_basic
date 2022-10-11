a694b6ffac62ae2eb4ade08632862ca14d6359b9
junsj119 <wjstjddud119@naver.com>
2022-10-11 12:28:42 +0900

    10/11

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.order.AppConfig;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        MemberService memberService = new MemberServiceImpl(memberRepository);
         Member member = new Member(1L, "memberA", Grade.VIP);
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
